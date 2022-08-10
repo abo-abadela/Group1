d15bae9f76701c19c58ae28c2a9e15981c63cabf
abo-abadela <abo3badela@gmail.com>
2022-08-10 13:11:56 +0200

    while file

---
+++ Contnu.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.contnu;
+
+/**
+ *
+ * @author almadkhal14
+ */
+public class Contnu {
+
+    public static void main(String[] args) {
+        int a=20;
+        while (a>=0 )
+        {
+            if (a==15 || a==7)
+                    {
+                        a--;
+                        continue;
+                    }
+        System.out.print(a + "  ");
+        a--;
+        }
+    }
+}
